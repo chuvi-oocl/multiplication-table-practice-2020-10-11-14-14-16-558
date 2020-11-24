d35bd7292a9ab2a08aaa30df02b8ef3cd12a8be2
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-24 16:53:14 +0800

    [MultiplicationTable][Victor] feat: generate multiplication table

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,43 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
+        if(isStartSmallerThanOrEqualToEnd(startNumber, endNumber) && isWithinRange(startNumber, endNumber)){
+            return getMultiplicationTable(startNumber, endNumber);
+        }
         return null;
+    }
+
+    private boolean isStartSmallerThanOrEqualToEnd(int startNumber, int endNumber){
+        return (startNumber <= endNumber);
+    }
+
+    private boolean isWithinRange(int startNumber, int endNumber) {
+        return ((startNumber >= 1 && startNumber <= 1000) && (endNumber >= 1 && endNumber <= 1000));
+    }
+
+    private String getMultiplicationTable(int startNumber, int endNumber){
+        String result = "";
+        for(int i = startNumber; i <= endNumber; i++){
+            if(result != ""){
+                result += "\r\n";
+            }
+            result += getMultiplicationLine(i, startNumber);
+        }
+        return result;
+    }
+
+    private String getMultiplicationLine(int rowNum, int startFrom){
+        String result = "";
+        for(int i = startFrom; i <= rowNum; i++){
+            if(result != ""){
+                result += "  ";
+            }
+            result += getMultiplicationResult(i, rowNum);
+        }
+        return result;
+    }
+
+    private String getMultiplicationResult(int numA, int numB){
+        return numA + "*" + numB + "=" + numA * numB;
     }
 }
